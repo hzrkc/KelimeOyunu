2796d9b2441342e5bc70576be77c4adb1da347f2
Hazar Koc <hazark41@gmail.com>
2023-04-23 15:07:43 +0300

    score.js düzenlendi,kelime listesi oluşturuldu. GitIgnore geçici düzenleme
    Signed-off-by: Hazar Koc <hazark41@gmail.com>

--- android/app/src/Score.js
+++ android/app/src/Score.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
+import kelimeListesi from './kelimeListesi'; // Kelime listesi dosyasını içe aktaralım
 
-const Score = ({ score }) => {
+const Score = ({ score, word }) => {
+  let matchingWordScore = 0;
+  
+  // Kelime listesindeki her kelime için kontrol yapalım
+  for (let i = 0; i < kelimeListesi.length; i++) {
+    // Girilen kelime kelime listesinde var mı diye kontrol edelim
+    if (word && kelimeListesi[i].toUpperCase() === word.toUpperCase()) {
+      // Kelime uzunluğuna göre puan hesaplayalım
+      const wordLength = word.length;
+      matchingWordScore = wordLength; // Kelime uzunluğu kadar puan verelim
+      
+      // Eşleşen kelime bulunduğunda döngüyü sonlandıralım
+      break;
+    }
+  }
+
   return (
     <Text style={styles.score}>
-      Skor: <Text style={styles.scoreNumber}>{score}</Text>
+      Skor: <Text style={styles.scoreNumber}>{score + matchingWordScore}</Text>
     </Text>
   );
 };
---
+++ android/app/src/kelimeListesi.js
@@ -0,0 +1,25 @@
+const kelimeListesi = [
+    "kitap",
+    "kalem",
+    "defter",
+    "masa",
+    "sandalye",
+    "bilgisayar",
+    "monitör",
+    "fare",
+    "klavye",
+    "hoparlör",
+    "süpürge",
+    "elektrikli süpürge",
+    "telefon",
+    "cep telefonu",
+    "telefon şarj cihazı",
+    "çanta",
+    "cüzdan",
+    "anahtar",
+    "kamera",
+    "televizyon"
+  ];
+  
+  export default kelimeListesi;
+  
