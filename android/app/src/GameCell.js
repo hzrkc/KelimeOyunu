4bc61b00904e131a8de56a53159bab2d077a7d79
Hazar Koc <hazark41@gmail.com>
2023-04-24 04:14:42 +0300

    Update GameCell.js

--- android/app/src/GameCell.js
+++ android/app/src/GameCell.js
@@ -1,30 +1,62 @@
-// Tek bir hücreyi temsil eden bileşenin kodları
+import React, { useRef } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Animated } from 'react-native';
 
-// React ve gerekli bileşenlerin dosyaya eklenmesi
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
-// GameCell bileşeni
 const GameCell = ({ value }) => {
+  const color = getRandomColor();
+
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  const handlePress = () => {
+    Animated.sequence([
+      Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animatedValue, {
+        toValue: -1,
+        duration: 600,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animatedValue, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
+
+  const interpolateRotation = animatedValue.interpolate({
+    inputRange: [-1, 1],
+    outputRange: ['-5deg', '5deg'],
+  });
+
   return (
-    <View style={styles.cell}>
-      <Text style={styles.letter}>{value}</Text>
-    </View>
+    <TouchableOpacity onPress={handlePress}>
+      <Animated.View style={[styles.cell, { backgroundColor: color, transform: [{ rotateZ: interpolateRotation }] }]}>
+        <Text style={styles.letter}>{value}</Text>
+      </Animated.View>
+    </TouchableOpacity>
   );
 };
 
-// Stiller
+const getRandomColor = () => {
+  const colors = ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800', '#FF5722', '#795548', '#9E9E9E', '#607D8B'];
+  return colors[Math.floor(Math.random() * colors.length)];
+};
+
 const styles = StyleSheet.create({
   cell: {
     borderWidth: 1,
     borderColor: '#ccc',
-    width: 30,
-    height: 30,
+    width: 40,
+    height: 40,
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 44,
   },
   letter: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: 'bold',
   },
 });
